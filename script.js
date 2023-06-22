ae25ee2eb17ebf7cf325200563455c3591ce9573
piush.bhandari <piush.bhandari@americaneagle.com>
2023-06-22 16:56:29 -0500

    markup and styles

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
- 
+//  theme switcher
+
+const themeBtns = [...document.querySelectorAll(".testbtn")];
+const body = document.querySelector("body");
+themeBtns.forEach((btn) => {
+  let dataTheme = btn.getAttribute("data-theme");
+
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    body.setAttribute("class", "");
+    body.classList.add(dataTheme);
+  });
+});
