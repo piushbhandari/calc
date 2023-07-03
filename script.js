1ecfc650ba9950bb8f9351701291cba6fe6d681b
piush.bhandari <piush.bhandari@americaneagle.com>
2023-07-03 15:22:42 -0500

    theme buttons

--- script.js
+++ script.js
@@ -2,15 +2,56 @@
 
 const themeBtns = [...document.querySelectorAll(".testbtn")];
 const body = document.querySelector("body");
+
+window.addEventListener("DOMContentLoaded", (e) => {
+  let theme = getStorage("theme");
+  let currentTheme = getStorage("currentThemeBtn");
+  let currentThemeBtn = document.querySelector(
+    `[data-theme="${currentTheme}"]`
+  );
+  if (theme) {
+    loadTheme(theme);
+  }
+  if (currentThemeBtn) {
+    toggleClasses(currentThemeBtn, themeBtns);
+  } else {
+    themeBtns[0].classList.add("active");
+  }
+});
+
 themeBtns.forEach((btn) => {
   let dataTheme = btn.getAttribute("data-theme");
 
   btn.addEventListener("click", (e) => {
     e.preventDefault();
-    body.setAttribute("class", "");
-    body.classList.add(dataTheme);
+    let currentBtn = e.currentTarget;
+
+    toggleClasses(currentBtn, themeBtns);
+    loadTheme(dataTheme);
+    setStorage("theme", dataTheme);
+    setStorage("currentThemeBtn", dataTheme);
   });
 });
+
+function loadTheme(dataTheme) {
+  body.setAttribute("class", "");
+  body.classList.add(dataTheme);
+}
+
+function toggleClasses(currentBtn, btns) {
+  btns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+
+  currentBtn.classList.add("active");
+}
+// local storage
+function setStorage(key, value) {
+  localStorage.setItem(key, value);
+}
+function getStorage(name) {
+  return localStorage.getItem(name);
+}
 
 // buttons
 
@@ -19,7 +60,7 @@ const symbolBtns = [...document.querySelectorAll(".symbol-btn")];
 const delBtn = document.querySelector(".backspace-btn");
 const equalBtn = document.querySelector(".equals-btn");
 const resetBtn = document.querySelector(".reset-btn");
-
+const dotBtn = document.querySelector(".dot-btn");
 // screen
 const screenTxt = document.querySelector(".calculator__screen-text");
 
@@ -38,6 +79,9 @@ numberBtns.forEach((btn) => {
 symbolBtns.forEach((btn) => {
   btn.addEventListener("click", processSymbol);
 });
+
+dotBtn.addEventListener("click", processDot);
+
 resetBtn.addEventListener("click", resetCalc);
 
 delBtn.addEventListener("click", deleteLastNum);
@@ -47,12 +91,9 @@ equalBtn.addEventListener("click", calculate);
 // functions
 function processNumber(e) {
   let currentBtn = e.currentTarget;
-  // if (screenTxt.textContent === "0") {
-  //   return;
-  // }
   let numberValue = currentBtn.value;
   currentNumber += numberValue;
-
+  console.log(currentNumber);
   updateScreen(currentNumber);
 }
 function processSymbol(e) {
@@ -60,9 +101,16 @@ function processSymbol(e) {
   currentSymbol = currentBtn.value;
   firstNumber = currentNumber;
   currentNumber = "";
-  screenTxt.textContent = currentSymbol;
+  updateScreen(currentSymbol, true);
 }
-
+function processDot(e) {
+  if (currentNumber.includes(".")) {
+    return;
+  } else {
+    currentNumber += ".";
+    updateScreen(currentNumber);
+  }
+}
 function deleteLastNum(e) {
   currentNumber = currentNumber.substring(0, currentNumber.length - 1);
   if (!currentNumber) {
@@ -88,8 +136,16 @@ function calculate(e) {
   } else if (currentSymbol === "/") {
     currentNumber = +firstNumber / +secondNumber;
   }
+
   updateScreen(currentNumber);
 }
-function updateScreen(value) {
-  screenTxt.textContent = value;
+function updateScreen(value, isSymbol = false) {
+  if (!isSymbol) {
+    screenTxt.textContent = formatNumber(value);
+  } else {
+    screenTxt.textContent = value;
+  }
+}
+function formatNumber(value) {
+  return Number.parseFloat(value).toLocaleString("en-US");
 }
